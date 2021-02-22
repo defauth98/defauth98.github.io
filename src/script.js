9ac31ee0959c6606cae047ae8200ecf8997ce45b
defauth98 <neto.daniribeiro@gmail.com>
2021-02-21 21:50:14 -0300

    Animaçao do menu lateral

---
+++ src/script.js
@@ -0,0 +1,53 @@
+const html = {
+  aboutContainer: document.querySelector('#about-container'),
+  aboutImg: document.querySelector('#about-img'),
+  aboutText: document.querySelector('#about-text'),
+
+  skillContainer: document.querySelector('#skill-container'),
+  skillImg: document.querySelector('#skill-img'),
+  skillText: document.querySelector('#skill-text'),
+
+  blogContainer: document.querySelector('#blog-container'),
+  blogImg: document.querySelector('#blog-img'),
+  blogText: document.querySelector('#blog-text'),
+};
+
+function hoverImg() {
+  html.aboutContainer.addEventListener('mouseenter', () => {
+    html.aboutImg.style.display = 'none';
+    html.aboutText.style.display = 'block';
+    html.aboutText.style.animation = 'move 1s';
+    html.aboutImg.style.animation = 'backMove 2.5s';
+  });
+
+  html.aboutContainer.addEventListener('mouseleave', () => {
+    html.aboutImg.style.display = 'block';
+    html.aboutText.style.display = 'none';
+  });
+
+  html.skillContainer.addEventListener('mouseenter', () => {
+    html.skillImg.style.display = 'none';
+    html.skillText.style.display = 'block';
+    html.skillText.style.animation = 'move 1s';
+    html.skillImg.style.animation = 'backMove 2.5s';
+  });
+
+  html.skillContainer.addEventListener('mouseleave', () => {
+    html.skillImg.style.display = 'block';
+    html.skillText.style.display = 'none';
+  });
+
+  html.blogContainer.addEventListener('mouseenter', () => {
+    html.blogImg.style.display = 'none';
+    html.blogText.style.display = 'block';
+    html.blogText.style.animation = 'move 1s';
+    html.blogImg.style.animation = 'backMove 2.5s';
+  });
+
+  html.blogContainer.addEventListener('mouseleave', () => {
+    html.blogImg.style.display = 'block';
+    html.blogText.style.display = 'none';
+  });
+}
+
+hoverImg();
