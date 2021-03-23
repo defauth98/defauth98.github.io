e7f7ede7649bca8c49882179b6226f827e5197a0
defauth98 <neto.daniribeiro@gmail.com>
2021-03-23 12:56:26 -0300

    Iniciando a animacao ao scrollar

--- src/script.js
+++ src/script.js
@@ -62,5 +62,19 @@ function animationOnClick(){
   })
 }
 
-hoverImg();
-animationOnClick()
+function animationOnScroll(){
+  const titleElement = document.querySelector('#section-skills h2');
+  const listElement = document.querySelector('#section-skills ul');
+  const scrollY = window.scrollY;
+
+  if(scrollY > 600){
+    titleElement.style.animationPlayState  = 'running';
+    listElement.style.animationPlayState  = 'running';
+  }
+}
+
+window.onload = () => {
+  hoverImg();
+  animationOnClick();
+  window.onscroll = animationOnScroll
+}
