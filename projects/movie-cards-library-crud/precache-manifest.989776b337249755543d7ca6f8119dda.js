15078236e4865210b56e620cc8b5d2517b15598f
defauth98 <neto.daniribeiro@gmail.com>
2021-05-07 22:00:24 -0300

    Adicionando o projeto

---
+++ projects/movie-cards-library-crud/precache-manifest.989776b337249755543d7ca6f8119dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b90c4e301d2b754b579fb9665acb9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f09b2c8dc1d153d28a4",
+    "url": "/static/css/main.bf3daac8.chunk.css"
+  },
+  {
+    "revision": "81ee25f15b399473cd57",
+    "url": "/static/js/2.3024a004.chunk.js"
+  },
+  {
+    "revision": "3f09b2c8dc1d153d28a4",
+    "url": "/static/js/main.293db1ce.chunk.js"
+  },
+  {
+    "revision": "2606678757bfc784b59e",
+    "url": "/static/js/runtime-main.707be6f4.js"
+  }
+]);
