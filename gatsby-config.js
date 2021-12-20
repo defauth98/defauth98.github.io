f2e0c7d924754085af9e783235bfef5c17ae9192
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-12-19 22:28:18 -0300

    feat: deploy config gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,30 @@
 module.exports = {
+  pathPrefix: 'defauth98.github.io',
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "defauth.github.io",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'defauth.github.io',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
