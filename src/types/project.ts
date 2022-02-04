33ce7a7ac173d723435807059bf21466b98220a0
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-02-04 19:12:08 -0300

    fix: project type

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,6 +2,13 @@ export default interface ProjectType {
   title: string;
   repoLink: string;
   thumbnailPath: string;
-  link: string
-  tags: string;
+  link: string;
+  tags: {
+      js: boolean,
+      ts: boolean,
+      html: boolean,
+      css: boolean,
+      react: boolean,
+      node: boolean,
+    },
 }
